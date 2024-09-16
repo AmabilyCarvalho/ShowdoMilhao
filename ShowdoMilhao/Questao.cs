380406791dba4fe4e8957a6b9119f251c6b30843
amabily <amabilymangolin123@gmail.com>
2024-09-16 09:05:17 -0300

    .

---
+++ ShowdoMilhao/Questao.cs
@@ -0,0 +1,21 @@
+namespace ShowdoMilhao;
+
+public class Questao
+{
+    public string pergunta;
+    public string resposta1;
+    public string resposta2;
+    public string resposta3;
+    public string resposta4;
+    public string resposta5;
+    private int respostacorreta;
+    public int nivel;
+    private int respostacorreta;
+
+    
+    public void desenhar()
+    {
+
+    }
+    public void 
+}
